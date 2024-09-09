7131713d11046f0ca4c2142bffe9fad7cfad9d3f
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-09 13:56:42 +0530

    Setup the db connection with server.js did the test run

--- serverSide/server.js
+++ serverSide/server.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 
 mongoose.connect("mongodb+srv://kaushikashehan123:ACjPmIqQKUguUmG4@cluster0.kho1z.mongodb.net/")
-.then(()=>console.log('Conected Successed')).catch(error=>console.log(error)); 
+.then(()=>console.log('Database Conected Successed')).catch(error=>console.log(error)); 
 
 const app = express()
 const PORT = process.env.PORT || 5000;
@@ -22,4 +22,8 @@ app.use(
         ],
         credentials : true
     })
-)
+);
+app.use(cookieParser());
+app.use(express.json);
+
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
