277a89e9c0da89a339a1592d63522b5761266002
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-13 14:42:51 +0530

    updated the code

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,6 +1,12 @@
 function AdminLayout() {
     return ( 
-        <div className="flex w-full min-h-screen"></div>
+        <div className="flex w-full min-h-screen">
+            <div className="flex flex-col flex-1">
+                <main className="flex flex-1 p-4 bg-muted/40 md:p-6 ">
+                    <Outlet/>
+                </main>
+            </div>
+        </div>
     );
 }
 
