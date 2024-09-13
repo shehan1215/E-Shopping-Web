7e763a7235cb462c938c2f04f0ed3e2293825a17
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-13 15:34:20 +0530

    fixed the error when run the UI not prompt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-
-import './App.css'
-import AuthLayout from './components/auth/layout'
-import AuthLogin from './pages/auth/login'
-import AuthRegister from './pages/auth/register'
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
 
 function App() {
 
@@ -10,9 +9,9 @@ function App() {
     <div className='flex flex-col overflow-hidden bg-white'>
       <h1>Header Component</h1>
       <Routes>
-        <Route path="/auth" element={<AuthLayout/>}>
-          <Route path="login" element={<AuthLogin/>}></Route>
-          <Route path="register" element={<AuthRegister/>}></Route>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<AuthLogin />}/>
+          <Route path="register" element={<AuthRegister />}/>
         </Route>
       </Routes>
     </div>
--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -7,7 +7,7 @@ const initialState = {
 }
 
 const authSlice = createSlice({
-    name : auth,
+    name : "auth",
     initialState,
     reducers : {
         setUser:(state,action)=>{
