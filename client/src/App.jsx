9235893da726ce366fbd5a6abb41d2f9fd003f66
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-15 09:55:14 +0530

    updated the admin-view file structure and fixed the error of layout of admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
 import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
 
 function App() {
 
@@ -14,7 +18,13 @@ function App() {
           <Route path="login" element={<AuthLogin />}/>
           <Route path="register" element={<AuthRegister />}/>
         </Route>
-        <Route path="/admin" element={<AdminLayout />}></Route>
+        
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />}/>
+          <Route path="products" element={<AdminProducts />}/>
+          <Route path="orders" element={<AdminOrders />}/>
+          <Route path="features" element={<AdminFeatures />}/>
+        </Route>
       </Routes>
     </div>
   )
--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AdminHeader from "./header";
 import AdminSidebar from "./sidebar";
 
@@ -6,6 +7,7 @@ function AdminLayout() {
         <div className="flex w-full min-h-screen">
             <AdminSidebar/>
             <div className="flex flex-col flex-1">
+                
                 <AdminHeader/>
                 <main className="flex flex-1 p-4 bg-muted/40 md:p-6 ">
                     <Outlet/>
