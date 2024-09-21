2d6885d8db574b23199a12304ab35069afaf29a7
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-21 09:32:30 +0530

    update the form controls

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,7 +1,15 @@
-function CommonForm() {
+import { Label } from "../ui/label";
+
+function CommonForm({formControls}) {
     return ( 
         <form>
-            
+            <div className="flex flex-col gap-3">
+                {
+                    formControls.map(controlItems=> <div className="grid w-full gap-1.5" key={controlItems.name}>
+                        <Label>{controlItems.label}</Label>
+                    </div>)
+                }
+            </div>
         </form>
     );
 }
