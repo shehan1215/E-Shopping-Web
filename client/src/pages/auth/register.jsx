f2b66c1b79619d4a617824c3254876975c78b9bf
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-23 08:48:56 +0530

    error fixed when import the link from react router dom

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 function AuthRegister() {
     return ( 
         <div className="mx-auto w-full max-w-md space-y-6">
             <div className="text-center">
                 <h1 className="text-3xl font-bold tracking-tighter text-foreground">Create New Account</h1>
+                <p className="mt-2">Already Have an Account
+                    <Link className="font-medium ml-2 text-primary hover:underline">
+                    Login</Link>
+                </p>
             </div>
         </div>
      );
