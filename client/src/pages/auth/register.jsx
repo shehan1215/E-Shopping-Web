6142b6f66307ba7d3994503b0eca08b0343b7602
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-23 08:50:00 +0530

    update the path

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -6,7 +6,7 @@ function AuthRegister() {
             <div className="text-center">
                 <h1 className="text-3xl font-bold tracking-tighter text-foreground">Create New Account</h1>
                 <p className="mt-2">Already Have an Account
-                    <Link className="font-medium ml-2 text-primary hover:underline">
+                    <Link className="font-medium ml-2 text-primary hover:underline" to="/auth/login">
                     Login</Link>
                 </p>
             </div>
