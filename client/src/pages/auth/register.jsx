31cf8e6ef4b91e06533d7a15c9887d7c35de23da
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-23 16:03:58 +0530

    updated

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,15 +1,27 @@
+import CommonForm from "@/components/common/form";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const initialState = {
+    userName : '',
+    email : '',
+    password : ''
+}
+
 function AuthRegister() {
+
+    const [formData,setFormData] = useState(initialState)
+
     return ( 
-        <div className="mx-auto w-full max-w-md space-y-6">
+        <div className="w-full max-w-md mx-auto space-y-6">
             <div className="text-center">
                 <h1 className="text-3xl font-bold tracking-tighter text-foreground">Create New Account</h1>
                 <p className="mt-2">Already Have an Account
-                    <Link className="font-medium ml-2 text-primary hover:underline" to="/auth/login">
+                    <Link className="ml-2 font-medium text-primary hover:underline" to="/auth/login">
                     Login</Link>
                 </p>
             </div>
+            <CommonForm/>
         </div>
      );
 }
