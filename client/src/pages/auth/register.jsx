85bf2371ab04c993dd8213cca6a0c138d7f29df1
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-23 08:39:54 +0530

    changed the reg page

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,6 +1,10 @@
 function AuthRegister() {
     return ( 
-        <div>register</div>
+        <div className="mx-auto w-full max-w-md space-y-6">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold tracking-tighter text-foreground">Create New Account</h1>
+            </div>
+        </div>
      );
 }
 
