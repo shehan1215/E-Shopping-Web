cc0d32a6a14eabd0d9ac3ecc9f3f11a0c49d8485
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-25 09:56:25 +0530

    update the auth-controller in server side

--- serverSide/controllers/auth/auth-controller.js
+++ serverSide/controllers/auth/auth-controller.js
@@ -1,6 +1,21 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const register = async(req, res)=>{
     const {userName, email, password} = req.body;
 
+    try {
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success : false,
+            message : "Error",
+        });
+    }
+}
+
+const login = async(req, res)=>{
     try {
         
     } catch (e) {
