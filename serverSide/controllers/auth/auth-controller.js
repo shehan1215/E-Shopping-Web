cc254e1c62e0d520cd0666daefa8e02dbaeefb25
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-09-25 10:06:10 +0530

    update the registration part in the server side

--- serverSide/controllers/auth/auth-controller.js
+++ serverSide/controllers/auth/auth-controller.js
@@ -1,11 +1,22 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../../models/User')
 
-const register = async(req, res)=>{
+const registerUser = async(req, res)=>{
     const {userName, email, password} = req.body;
 
     try {
-        
+        const hashPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            userName,
+            email,
+            password : hashPassword
+        })
+        await newUser.save()
+        res.status(200).json({
+            success : true,
+            message : "register successed",
+        });
     } catch (e) {
         console.log(e);
         res.status(500).json({
@@ -26,3 +37,6 @@ const login = async(req, res)=>{
         });
     }
 }
+
+
+module.exports = {registerUser}
