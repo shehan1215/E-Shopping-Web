2d18fa742dd6ec6b0d41dba5fa2d1a689ca4b732
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-10-01 10:04:02 +0530

    fixed the database update error

--- serverSide/server.js
+++ serverSide/server.js
@@ -7,25 +7,25 @@ const authRouter = require('./routes/auth/auth-routes')
 mongoose.connect("mongodb+srv://kaushikashehan123:ACjPmIqQKUguUmG4@cluster0.kho1z.mongodb.net/")
 .then(()=>console.log('Database Conected Successed')).catch(error=>console.log(error)); 
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin : "http://localhost:5173",
-        methods : ["GET", "POST", "DELETE", "PUT"],
-        allowedHeaders : [
-            "Content-Type",
-            'Authorization',
-            'Cache-Control',
-            'Expires',
-            'Pragma'
-        ],
-        credentials : true
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
+      credentials: true,
     })
-);
+  );
 app.use(cookieParser());
-app.use(express.json);
-app.use('/api/auth', authRouter)
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
