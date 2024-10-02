356ec2277c82905d68fbbd763c63a1cf51ab5abd
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-10-02 09:27:26 +0530

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { Toast } from './components/ui/toast.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
       <App/>
+      <Toast/>
     </Provider>
   </BrowserRouter>
 )
