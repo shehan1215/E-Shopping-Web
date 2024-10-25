9314a2bca302954b1ae65435c376ddd075a14d02
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-10-25 09:14:42 +0530

    update login user state

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -63,8 +63,8 @@ const authSlice = createSlice({
             console.log(action);
             
             state.isLoading = false;
-            state.user = !action.payload.success? null : action.payload.user;
-            state.isAuthenticated = !action.payload.success?false : true;
+            state.user = action.payload.success? action.payload.user : null;
+            state.isAuthenticated = action.payload.success;
           })
           .addCase(loginUser.rejected, (state, action) => {
             state.isLoading = false;
